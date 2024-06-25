4fe45ffd2e56d42d7db5c0afca0a7973ab426eaf
Leonard Mazzone <lenny@tetricuslabs.com>
2024-06-25 08:49:09 -0400

    dont use settings

--- src/generators.py
+++ src/generators.py
@@ -1,13 +1,14 @@
-def generate_cache_key(cache_prefix: str, item_primary_key: str) -> str:
+def generate_cache_key(cache_prefix: str, item_primary_key: str, cache_postfix:str) -> str:
     """Generate a unique cache key with a prefix and key.
     :param cache_prefix: The prefix to use for the cache key, e.g. "feed_item_content"
     :param item_primary_key: The primary key of the item to cache
+    :param cache_postfix: Optional postfix to use for the cache key, e.g. "staging"
     :return: The generated cache key
     """
     cache_key = (
-        f"{cache_prefix}_{item_primary_key}"
-        if settings.working_env == Environments.PRODUCTION
-        else f"{cache_prefix}_{item_primary_key}_staging"
+        f"{cache_prefix}_{item_primary_key}_{cache_postfix}"
+        if cache_postfix
+        else f"{cache_prefix}_{item_primary_key}"
     )
 
     return cache_key
